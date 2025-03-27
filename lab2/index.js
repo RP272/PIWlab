d3e1c161a469f30e160a8553a403829c96b0731d
robibobi <robibobi@DESKTOP-322IV4E>
2025-03-27 09:48:33 +0100

    Displaying completion date is working.

--- lab2/index.js
+++ lab2/index.js
@@ -10,10 +10,26 @@ addTaskButton.addEventListener("click", () => {
         return;
     }
     let newTask = document.createElement("li");
-    newTask.innerText = taskDesc;
+    newTask.innerHTML = `<span class="taskDesc">${taskDesc}</span>`;
     list1.appendChild(newTask);
 })
 
+const leadingZero = (number) => {
+    return number < 10 ? `0${number}` : number;
+}
+
 list1.addEventListener("click", (e) => {
-    e.target.classList.toggle("done")
+    if(e.target.classList.contains("taskDesc") === false) return;
+    e.target.parentElement.classList.toggle("done")
+    if(e.target.parentElement.classList.contains("done") === true){
+        let currentDate = new Date();
+        let day = leadingZero(currentDate.getDate());
+        let month = leadingZero(currentDate.getMonth());
+        let hours = leadingZero(currentDate.getHours());
+        let minutes = leadingZero(currentDate.getMinutes());
+        let seconds = leadingZero(currentDate.getSeconds());
+        e.target.parentElement.innerHTML = e.target.parentElement.innerHTML + ` <span class="taskCompletionDate">${day}-${month}-${currentDate.getFullYear()} ${hours}:${minutes}:${seconds}</span>`
+    }else{
+        e.target.parentElement.removeChild(e.target.parentElement.getElementsByClassName("taskCompletionDate")[0])
+    }
 })
