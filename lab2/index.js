fb7f5841dbe50b39fe0f1ffc76dba7b6efac87fd
robibobi <robibobi@DESKTOP-322IV4E>
2025-03-27 08:54:33 +0100

    Created new folder for lab2. Added index.html, style.css, index.js files to it. Basic page structure. Addding task to the list is working.

---
+++ lab2/index.js
@@ -0,0 +1,15 @@
+const addTaskButton = document.getElementById("addTaskButton");
+const taskInput = document.getElementById("taskInput");
+const taskLists = document.getElementById("taskLists");
+const list1 = document.getElementById("list1");
+
+addTaskButton.addEventListener("click", () => {
+    let taskDesc = taskInput.value;
+    if(taskDesc.length === 0){
+        alert("Task description can't be empty. Fill the input and try again!")
+        return;
+    }
+    let newTask = document.createElement("li");
+    newTask.innerText = taskDesc;
+    list1.appendChild(newTask);
+})
