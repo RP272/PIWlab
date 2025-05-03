3f30e2378d62d727b8573d05e9ac9fda74ed6e11
RP272 <148786081+RP272@users.noreply.github.com>
2025-05-03 20:42:39 +0200

    Update init.js

--- lab3/app/data/init.js
+++ lab3/app/data/init.js
@@ -3,15 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDsemzegdg1laagDcddpLAfW9wc3Kzs1SI",
-    authDomain: "piwo3labmoj123.firebaseapp.com",
-    projectId: "piwo3labmoj123",
-    storageBucket: "piwo3labmoj123.firebasestorage.app",
-    messagingSenderId: "229919185088",
-    appId: "1:229919185088:web:4cfcb5dad69878d151f525",
-    measurementId: "G-065BVS4JY6"
-  };
+const firebaseConfig = {};
 
 const app = initializeApp(firebaseConfig);
 
